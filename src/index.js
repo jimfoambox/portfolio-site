af2b1896559a8eec1b7f5fa5be5cbd304f2b8c3e
James Gill <jamesgill347@gmail.com>
2021-02-18 15:25:06 -0500

    updated index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import Routes from './components/Routes'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../public/styles.css'
 
-
 render(
     <Routes />,
   document.getElementById('main')
