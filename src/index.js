2d1c802f0256f612b33e2a10ff3a7241db83f650
James Gill <jamesgill347@gmail.com>
2021-02-18 15:06:03 -0500

    package.json updated with node version.  redux provider removed from index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import React from 'react'
 import Routes from './components/Routes'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../public/styles.css'
-// import {Provider} from 'react-redux'
-// import store from './store'
+
 
 render(
-//   <Provider store={store}>
     <Routes />,
-//   </Provider>,
   document.getElementById('main')
 )
